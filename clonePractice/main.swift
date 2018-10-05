9bf72e5f9cd63bf5c1fdf23eb58b77e633c955d1
alysonabril <alysonabril@pursuit.org>
2018-10-05 15:30:01 -0400

    first attempt

---
+++ clonePractice/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  clonePractice
+//
+//  Created by Alyson Abril on 10/5/18.
+//  Copyright © 2018 Alyson Abril. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let test = true
+if test == true {
+    print("success")
+} else {
+    print("this sucks")
+}
